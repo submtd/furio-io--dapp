aa87084c3c48389de1aa981d85fa546f07103aa4
Steve Harmeyer <harmeyersteve@gmail.com>
2022-04-22 13:18:13 -0700

    try wss

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import useSetings from "./composables/useSettings";
+useSetings();
 createApp({
     components: {
         App,
