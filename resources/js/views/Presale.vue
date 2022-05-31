affd484a20d5192158e4d7018513b8bed15016e7
Steve Harmeyer <harmeyersteve@gmail.com>
2022-05-31 09:17:16 -0700

    presale is over

--- resources/js/views/Presale.vue
+++ resources/js/views/Presale.vue
@@ -1,3 +1,6 @@
 <template>
-    <h1>All Presales Have Ended</h1>
+    <div class="bg-light text-dark rounded p-5">
+        <h1>All Presales Have Ended</h1>
+        <p>Please come back on June 14th for the project launch!</p>
+    </div>
 </template>
