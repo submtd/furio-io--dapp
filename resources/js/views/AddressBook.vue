23d5a4a18ed3f688898a5c956266d8b1c347c88e
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-01 13:54:32 -0700

    update settings on login

--- resources/js/views/AddressBook.vue
+++ resources/js/views/AddressBook.vue
@@ -8,26 +8,26 @@
         <div v-show="store.state.wallet.loggedIn">
             <dl class="row">
                 <dt class="col-sm-3">Claim Contract</dt>
-                <dd class="col-sm-9"><a href="{{ store.state.settings.block_explorer_url }}/address/{{ store.state.settings.claim_address }}">{{ store.state.settings.claim_address }}</a></dd>
+                <dd class="col-sm-9"><a :href="claimLink">{{ store.state.settings.claim_address }}</a></dd>
                 <dt class="col-sm-3">Downline NFT Contract</dt>
-                <dd class="col-sm-9"><a href="{{ store.state.settings.block_explorer_url }}/address/{{ store.state.settings.downline_address }}">{{ store.state.settings.downline_address }}</a></dd>
+                <dd class="col-sm-9"><a :href="downlineLink">{{ store.state.settings.downline_address }}</a></dd>
                 <dt class="col-sm-3">Presale NFT Contract</dt>
-                <dd class="col-sm-9"><a href="{{ store.state.settings.block_explorer_url }}/address/{{ store.state.settings.presale_address }}">{{ store.state.settings.presale_address }}</a></dd>
+                <dd class="col-sm-9"><a :href="presaleLink">{{ store.state.settings.presale_address }}</a></dd>
                 <dt class="col-sm-3">Swap Contract</dt>
-                <dd class="col-sm-9"><a href="{{ store.state.settings.block_explorer_url }}/address/{{ store.state.settings.swap_address }}">{{ store.state.settings.swap_address }}</a></dd>
+                <dd class="col-sm-9"><a :href="swapLink">{{ store.state.settings.swap_address }}</a></dd>
                 <dt class="col-sm-3">Token Contract</dt>
-                <dd class="col-sm-9"><a href="{{ store.state.settings.block_explorer_url }}/address/{{ store.state.settings.token_address }}">{{ store.state.settings.token_address }}</a></dd>
+                <dd class="col-sm-9"><a :href="tokenLink">{{ store.state.settings.token_address }}</a></dd>
                 <dt class="col-sm-3">Payment Contract</dt>
-                <dd class="col-sm-9"><a href="{{ store.state.settings.block_explorer_url }}/address/{{ store.state.settings.payment_address }}">{{ store.state.settings.payment_address }}</a></dd>
+                <dd class="col-sm-9"><a :href="paymentLink">{{ store.state.settings.payment_address }}</a></dd>
                 <dt class="col-sm-3">Vault Contract</dt>
-                <dd class="col-sm-9"><a href="{{ store.state.settings.block_explorer_url }}/address/{{ store.state.settings.vault_address }}">{{ store.state.settings.vault_address }}</a></dd>
+                <dd class="col-sm-9"><a :href="vaultLink">{{ store.state.settings.vault_address }}</a></dd>
             </dl>
         </div>
     </div>
 </template>
 
 <script>
-import { onMounted } from "vue";
+import { rel, onMounted } from "vue";
 import { useStore } from "vuex";
 import useSettings from "../composables/useSettings";
 export default {
@@ -35,12 +35,27 @@ export default {
         const store = useStore();
         const settings = useSettings();
 
+        const claimLink = rel(store.state.settings.block_explorer_url + '/address/' + store.state.settings.claim_address);
+        const downlineLink = rel(store.state.settings.block_explorer_url + '/address/' + store.state.settings.claim_address);
+        const presaleLink = rel(store.state.settings.block_explorer_url + '/address/' + store.state.settings.claim_address);
+        const swapLink = rel(store.state.settings.block_explorer_url + '/address/' + store.state.settings.claim_address);
+        const tokenLink = rel(store.state.settings.block_explorer_url + '/address/' + store.state.settings.claim_address);
+        const paymentLink = rel(store.state.settings.block_explorer_url + '/address/' + store.state.settings.claim_address);
+        const vaultLink = rel(store.state.settings.block_explorer_url + '/address/' + store.state.settings.claim_address);
+
         onMounted(async () => {
             settings.update();
         });
 
         return {
             store,
+            claimLink,
+            downlineLink,
+            presaleLink,
+            swapLink,
+            tokenLink,
+            paymentLink,
+            vaultLink,
         }
     }
 }
