fe075753d8886ed1d29cb64701e944d8f2dd2357
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-01 08:01:58 -0700

    abi helper class

--- app/Abis/Abi.php
+++ app/Abis/Abi.php
@@ -2,18 +2,36 @@
 
 namespace App\Abis;
 
-use stdClass;
-
 abstract class Abi
 {
     /**
      * Invoke.
      *   Returns the abi string for a smart contract.
      *
+     * @return string
+     */
+    public function __invoke(): string
+    {
+        return $this->abi ?? '[]';
+    }
+
+    /**
+     * To array
+     *
      * @return array
      */
-    public function __invoke(): array
+    public static function toArray(): array
     {
-        return json_decode($this->abi ?? '[]');
+        return json_decode((new static)());
+    }
+
+    /**
+     * To string
+     *
+     * @return string
+     */
+    public static function toString(): string
+    {
+        return (new static)();
     }
 }
