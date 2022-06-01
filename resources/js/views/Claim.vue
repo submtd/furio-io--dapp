78a7739837f6f7cf45be174a4bb2cad143181ab7
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-01 16:33:32 -0700

    added claim abi

--- resources/js/views/Claim.vue
+++ resources/js/views/Claim.vue
@@ -21,7 +21,7 @@ export default {
 
         const getNfts = async () => {
             try {
-                const contract = web3.eth.Contract(JSON.parse(store.state.settings.claim_abi), store.state.settings.claim_address);
+                const contract = new web3.eth.Contract(JSON.parse(store.state.settings.claim_abi), store.state.settings.claim_address);
                 const nfts = await contract.methods.owned(store.state.wallet.address).call();
                 console.log(nfts);
             } catch (error) {
