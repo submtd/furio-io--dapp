e2627a399b3c5ba2db18fe2dfb8ada575928b725
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-02 08:48:25 -0700

    redirect to connect page

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,40 +1,34 @@
 <template>
-    <nav class="navbar navbar-dark navbar-expand-xl pt-4">
+    <nav v-show="store.state.wallet.loggedIn" class="navbar navbar-dark navbar-expand-xl pt-4">
         <div class="container">
-            <div v-show="store.state.wallet.loggedIn">
-                <router-link :to="{ name: 'Home' }" class="navbar-brand">
-                    <img class="d-none d-lg-block" src="../../images/furio-logo.svg" alt="Furio Logo" height="60"/>
-                    <img class="d-lg-none" src="../../images/furio-icon.svg" alt="Furio Icon" height="60"/>
-                </router-link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbar">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'Home' }" class="nav-link" active-class="active">Home</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'Claim' }" class="nav-link" active-class="active">Claim</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'Swap' }" class="nav-link" active-class="active">Swap</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'Vault' }" class="nav-link" active-class="active">Vault</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'Downline' }" class="nav-link" active-class="active">Downline</router-link>
-                        </li>
-                    </ul>
-                    <form class="d-flex">
-                        <button @click="wallet.disconnect" class="btn btn-danger">DISCONNECT {{ store.state.wallet.shortAddress }}</button>
-                    </form>
-                </div>
-            </div>
-            <div v-show="!store.state.wallet.loggedIn">
+            <router-link :to="{ name: 'Home' }" class="navbar-brand">
                 <img class="d-none d-lg-block" src="../../images/furio-logo.svg" alt="Furio Logo" height="60"/>
                 <img class="d-lg-none" src="../../images/furio-icon.svg" alt="Furio Icon" height="60"/>
+            </router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'Home' }" class="nav-link" active-class="active">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'Claim' }" class="nav-link" active-class="active">Claim</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'Swap' }" class="nav-link" active-class="active">Swap</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'Vault' }" class="nav-link" active-class="active">Vault</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'Downline' }" class="nav-link" active-class="active">Downline</router-link>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <button @click="wallet.disconnect" class="btn btn-danger">DISCONNECT {{ store.state.wallet.shortAddress }}</button>
+                </form>
             </div>
         </div>
     </nav>
