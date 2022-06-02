12c6ebf6a1ea07b56c556aa56ef2e8ec7aeff7e3
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-02 12:12:07 -0700

    swap

--- resources/js/views/Swap.vue
+++ resources/js/views/Swap.vue
@@ -1,6 +1,13 @@
 <template>
+    <h1>Swap</h1>
     <div class="bg-light text-dark rounded p-5">
-        <h1>Swap</h1>
-        <p>Please come back on June 14th for the project launch!</p>
+        <div class="row">
+            <div class="col-md-5 mb-2">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo similique hic corporis laudantium provident numquam culpa autem libero maxime veritatis, nulla dignissimos necessitatibus itaque dicta beatae quos repellat asperiores quas.</p>
+            </div>
+            <div class="col-md-7 mb-2">
+
+            </div>
+        </div>
     </div>
 </template>
