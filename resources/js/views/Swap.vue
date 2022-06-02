c67a6005b48e17a03bedd9a8ca56af0f7e38cae2
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-02 12:14:33 -0700

    swap

--- resources/js/views/Swap.vue
+++ resources/js/views/Swap.vue
@@ -6,7 +6,14 @@
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo similique hic corporis laudantium provident numquam culpa autem libero maxime veritatis, nulla dignissimos necessitatibus itaque dicta beatae quos repellat asperiores quas.</p>
             </div>
             <div class="col-md-7 mb-2">
-
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Swap</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Buy</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
