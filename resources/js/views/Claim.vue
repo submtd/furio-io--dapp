fbc9e8f5b1928a39790c51b9085bd3866c174b55
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-02 08:57:58 -0700

    redirect to connect page

--- resources/js/views/Claim.vue
+++ resources/js/views/Claim.vue
@@ -1,17 +1,15 @@
 <template>
     <div class="bg-light text-dark rounded p-5">
         <h1>Claim</h1>
-        <div class="row">
-            <div v-show="!available">
-                <p>You do not have any $FUR tokens available to claim.</p>
+        <div v-show="!available" class="row">
+            <p>You do not have any $FUR tokens available to claim.</p>
+        </div>
+        <div v-show="available" class="row">
+            <div class="col-md-6">
+                <p>You have <strong>{{ available }}</strong> $FUR tokens available to claim.</p>
             </div>
-            <div v-show="available">
-                <div class="col-md-6">
-                    <p>You have <strong>{{ available }}</strong> $FUR tokens available to claim.</p>
-                </div>
-                <div class="col-md-6">
-                    <input v-model="quantity" :max="available" min="0" type="number" class="form-control mb-2" id="quantity"/>
-                </div>
+            <div class="col-md-6">
+                <input v-model="quantity" :max="available" min="0" type="number" class="form-control mb-2" id="quantity"/>
             </div>
         </div>
     </div>
