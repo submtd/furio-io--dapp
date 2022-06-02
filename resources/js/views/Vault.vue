fff4539b739df7eb0b6fa08cdabf04cc4bad8e3d
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-02 16:03:53 -0700

    vault

--- resources/js/views/Vault.vue
+++ resources/js/views/Vault.vue
@@ -1,6 +1,47 @@
 <template>
-    <div class="bg-light text-dark rounded p-5">
-        <h1>Vault</h1>
-        <p>Please come back on June 14th for the project launch!</p>
+    <h1>Vault</h1>
+    <div class="row flex-row-reverse gx-5">
+        <div class="col-lg-7 bg-light text-dark rounded p-5 mb-4">
+        </div>
+        <div class="col-lg-5">
+            <div class="row">
+                <div class="col-lg-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <p class="card-title">Initial Deposit</p>
+                            <p class="card-text"><strong>0 $FUR</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <p class="card-title">Total Deposit</p>
+                            <p class="card-text"><strong>0 $FUR</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <p class="card-title">Claimed</p>
+                            <p class="card-text"><strong>0 $FUR</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <p class="card-title">Direct Referrals</p>
+                            <p class="card-text"><strong>0</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {}
+</script>
