b6f32605ed986eaa39e319c923d5c15776ed6534
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-06 08:53:02 -0700

    added vault abi

--- app/Services/SettingsService.php
+++ app/Services/SettingsService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Abis\AddressBook;
 use App\Abis\Claim;
+use App\Abis\Vault;
 use App\Models\Setting;
 
 class SettingsService
@@ -23,6 +24,7 @@ class SettingsService
         $settings['server_time'] = now()->timestamp;
         $settings['addressbook_abi'] = AddressBook::toString();
         $settings['claim_abi'] = Claim::toString();
+        $settings['vault_abi'] = Vault::toString();
 
         return $settings;
     }
