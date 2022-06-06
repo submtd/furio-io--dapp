5e098ade8ddea68d781317029d565da371f098de
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-06 13:05:48 -0700

    bigint overflow

--- resources/js/composables/useDisplayCurrency.js
+++ resources/js/composables/useDisplayCurrency.js
@@ -1,6 +1,6 @@
 export default () => {
     const format = (amount) => {
-        return Math.floor(BigInt(amount) / BigInt("1000000000000000")) / BigInt("1000");
+        return Math.floor(amount / BigInt("1000000000000000")) / BigInt("1000");
     }
 
     return {
