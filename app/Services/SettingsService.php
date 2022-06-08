3fd5b1625c3f0e75bfc6ffc69ea06f0036be7e87
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-08 11:16:41 -0700

    Downline abi added

--- app/Services/SettingsService.php
+++ app/Services/SettingsService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Abis\AddressBook;
 use App\Abis\Claim;
+use App\Abis\Downline;
 use App\Abis\Payment;
 use App\Abis\Token;
 use App\Abis\Vault;
@@ -26,6 +27,7 @@ class SettingsService
         $settings['server_time'] = now()->timestamp;
         $settings['addressbook_abi'] = AddressBook::toString();
         $settings['claim_abi'] = Claim::toString();
+        $settings['downline_abi'] = Downline::toString();
         $settings['payment_abi'] = Payment::toString();
         $settings['token_abi'] = Token::toString();
         $settings['vault_abi'] = Vault::toString();
