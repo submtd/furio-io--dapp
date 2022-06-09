43a3e9e517cf95eed9c4286eeaca8fffe84556bc
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-08 17:44:21 -0700

    downline

--- resources/js/views/Downline.vue
+++ resources/js/views/Downline.vue
@@ -8,6 +8,13 @@
                 </div>
             </div>
             <div v-show="!loading">
+                <div v-show="owned < 15">
+                    <div class="form-group">
+                        <label for="buy-quantity">Quantity</label>
+                        <input v-model="buyQuantity" class="form-control" id="buy-quantity"/>
+                    </div>
+                    <button @click="buy" class="btn btn-lg btn-info btn-block mb-2">Buy</button>
+                </div>
             </div>
         </div>
         <div class="col-lg-5">
@@ -45,6 +52,7 @@ export default {
         const loading = ref(false);
         const totalSupply = ref(0);
         const owned = ref(0);
+        const buyQuantity = ref(0);
 
         onMounted(async () => {
             await update();
@@ -60,16 +68,23 @@ export default {
                 const contract = downlineContract();
                 totalSupply.value = await contract.methods.totalSupply().call();
                 owned.value = await contract.methods.balanceOf(store.state.wallet.address).call();
+                buyQuantity.value = 15 - owned.value;
             } catch (error) {
                 alerts.danger(error.message);
             }
             loading.value = false;
         }
 
+        const buy = async () => {
+
+        }
+
         return {
             loading,
             totalSupply,
             owned,
+            buyQuantity,
+            buy,
         }
     }
 
