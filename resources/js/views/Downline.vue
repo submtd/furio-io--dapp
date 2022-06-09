b2e9434c9ef0aeb6e25684a07ad1fd6acd7dd60a
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-08 17:33:31 -0700

    downline

--- resources/js/views/Downline.vue
+++ resources/js/views/Downline.vue
@@ -1,6 +1,53 @@
 <template>
-    <div class="bg-light text-dark rounded p-5">
-        <h1>Downline</h1>
-        <p>Please come back on June 14th for the project launch!</p>
+    <h1>Downline</h1>
+    <div class="row flex-row-reverse gx-5">
+        <div class="col-lg-7 bg-light text-dark rounded p-5 mb-4">
+            <div v-show="loading" class="text-center">
+                <div class="spinner-border m-5" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+            <div v-show="!loading">
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <div class="row">
+                <div class="col-lg-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <p class="card-title">Balance</p>
+                            <p class="card-text"><strong>{{ depositedDisplay }} $FUR</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <p class="card-title">Claimed</p>
+                            <p class="card-text"><strong>{{ claimedDisplay }} $FUR</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <p class="card-title">Reward Rate</p>
+                            <p class="card-text"><strong>{{ rewardRate }}%</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <p class="card-title">Participant Status</p>
+                            <p class="card-text"><strong>{{ participantStatusDisplay }}</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+</script>
