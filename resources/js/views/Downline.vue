793957dc238f69e6540725fa9daa510e82f4f294
Steve Harmeyer <harmeyersteve@gmail.com>
2022-06-27 08:58:41 -0700

    521 fix?

--- resources/js/views/Downline.vue
+++ resources/js/views/Downline.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>Downline NFTs</h1>
-    <p v-show="!isSelf" class="mb-t">Buy and sell downline NFT's here.</p>
+    <p class="mb-t">Buy and sell downline NFT's here.</p>
     <div class="row flex-row-reverse gx-5">
         <div class="col-lg-7 bg-light text-dark rounded p-5 mb-4">
             <div v-show="loading" class="text-center">
@@ -9,7 +9,7 @@
                 </div>
             </div>
             <div v-show="!loading">
-                <div v-show="isSelf" class="row mb-3">
+                <div class="row mb-3">
                     <div class="col-md-6">
                         <h5>Buy Downline NFTs</h5>
                         <div class="form-group">
@@ -55,24 +55,18 @@
 </template>
 
 <script>
-import { ref, computed, onMounted, watch } from "vue";
-import router from "../router";
+import { ref, computed, onMounted } from "vue";
 import { useStore } from "vuex";
-import { useRoute } from "vue-router";
 import useBalances from "../composables/useBalances";
 import useAlerts from "../composables/useAlerts";
 import useDisplayCurrency from '../composables/useDisplayCurrency';
-import useWallet from "../composables/useWallet";
 
 export default {
     setup () {
         const store = useStore();
         const alerts = useAlerts();
-        const route = useRoute();
         const balances = useBalances();
         const displayCurrency = useDisplayCurrency();
-        const wallet = useWallet();
-        const address = ref(null);
         const loading = ref(false);
         const maxSupply = ref(0);
         const totalSupply = ref(0);
@@ -80,47 +74,7 @@ export default {
         const buyQuantity = ref(0);
         const sellQuantity = ref(0);
         const participant = ref(null);
-        const referrals = ref([]);
-        const airdropAmount = ref(0);
-        const minBalance = ref(0);
-        const maxBalance = ref(100000);
         const walletBalance = ref(0);
-        const teamaddress = ref(null);
-        const shortteamaddress = ref(null);
-        const showUpdateNameForm = ref(false);
-        const updatedName = ref(null);
-        const rewardRate = ref(0);
-        const participantStatus = ref(1);
-        const lookupTeam = ref(null);
-        const individualAirdropAmount = ref(0);
-        const showUpdateReferrerForm = ref(false);
-        const newReferrer = ref(null);
-        const showSocialMediaForm = ref(false);
-        const twitter = ref(null);
-        const telegram = ref(null);
-        const discord = ref(null);
-        const medium = ref(null);
-        const facebook = ref(null);
-        const instagram = ref(null);
-        const showImageUploadForm = ref(false);
-        const image = ref(null);
-
-        const name = computed(() => {
-            if(!address.value) {
-                return null;
-            }
-            return address.value.attributes.name ?? address.value.attributes.shortAddress;
-        });
-
-        const avatar = computed(() => {
-            if(!address.value) {
-                return "/profile.png"
-            }
-            if(typeof address.value.related == "undefined") {
-                return "/profile.png";
-            }
-            return address.value.related.avatar.attributes.path;
-        });
 
         const available = computed(() => {
             const remaining = maxSupply.value - totalSupply.value;
@@ -138,138 +92,13 @@ export default {
             return participant.value.directReferrals;
         });
 
-        const rewarded = computed(() => {
-            if(!participant.value) {
-                return 0;
-            }
-            return displayCurrency.format(participant.value.awarded);
-        });
-
-        const teamWallet = computed(() => {
-            let tw = "No";
-            if(!participant.value) {
-                return tw;
-            }
-            if(participant.value.teamWallet) {
-                tw = "Yes";
-            }
-            return tw;
-        });
-
-        const referrer = computed(() => {
-            if(!participant.value) {
-                return null;
-            }
-            return participant.value.referrer;
-        });
-
-        const isSelf = computed(() => {
-            if(!address.value) {
-                return false;
-            }
-            if(typeof store.state.wallet == "undefined") {
-                return false;
-            }
-            return address.value.attributes.address == store.state.wallet.address;
-        });
-
-        const participantStatusDisplay = computed(() => {
-            switch(participantStatus.value) {
-                case "1":
-                    return 'Negative';
-                case "2":
-                    return 'Neutral';
-                case "3":
-                    return 'Positive';
-            }
-        });
-
-        const canUpdateReferrer = computed(() => {
-            if(!participant.value) {
-                return false;
-            }
-            if(participant.value.referrer == store.state.settings.safe_address && participant.value.directReferrals == 0) {
-                return true;
-            }
-            return false;
-        });
-
         addEventListener("refresh", async () => {
-            if(!address.value) {
-                //await update();
-            }
-        });
-
-        const addr = computed(async () => {
-            return route.params.teamaddress ?? store.state.wallet.address;
-        });
-
-        watch(addr, async () => {
             await update();
         });
 
         onMounted(async () => {
             await update();
         });
-
-        const participantLink = (address) => {
-            router.push("/team/" + address);
-        }
-
-        const showSocialMenu = computed(() => {
-            if(!address.value) {
-                return false;
-            }
-            return socialLink("twitter") ||
-                   socialLink("telegram") ||
-                   socialLink("discord") ||
-                   socialLink("medium") ||
-                   socialLink("facebook") ||
-                   socialLink("instagram");
-        });
-
-        const socialLink = (platform) => {
-            if(!address.value) {
-                return false;
-            }
-            if(!address.value.attributes[platform]) {
-                return false;
-            }
-            return address.value.attributes[platform];
-        }
-
-        const toggleImageUploadForm = () => {
-            showImageUploadForm.value = !showImageUploadForm.value;
-        }
-
-        const uploadImage = async (e) => {
-            let files = e.target.files || e.dataTransfer.files;
-            if (!files.length) return;
-            const signature = await web3.eth.personal.sign(address.value.attributes.nonce, address.value.attributes.address, "");
-            const formData = new FormData();
-            formData.append("address", address.value.attributes.address);
-            formData.append("nonce", address.value.attributes.nonce);
-            formData.append("signature", signature);
-            formData.append("image", files[0]);
-            await axios.post("/api/v1/image", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data"
-                }
-            }).then(response => {
-                alerts.info("Team image updated");
-            }).catch(error => {
-                if(error.response.status == 422) {
-                    alerts.danger(error.response.data.message);
-                    return;
-                }
-                alerts.danger(error.message);
-            })
-        }
-
-        const toggleSocialMediaForm = () => {
-            showSocialMediaForm.value = !showSocialMediaForm.value;
-        }
-
 
         const downlineContract = () => {
             return new web3.eth.Contract(JSON.parse(store.state.settings.downline_abi), store.state.settings.downline_address);
@@ -286,81 +115,18 @@ export default {
         const update = async () => {
             loading.value = true;
             try {
-                address.value = await wallet.lookupAddress(route.params.teamaddress ?? store.state.wallet.address);
-                twitter.value = address.value.attributes.twitter;
-                telegram.value = address.value.attributes.telegram;
-                discord.value = address.value.attributes.discord;
-                medium.value = address.value.attributes.medium;
-                facebook.value = address.value.attributes.facebook;
-                instagram.value = address.value.attributes.instagram;
                 const contract = downlineContract();
                 const vault = vaultContract();
-                const token = tokenContract();
                 totalSupply.value = await contract.methods.totalSupply().call();
                 maxSupply.value = await contract.methods.maxSupply().call();
-                owned.value = await contract.methods.balanceOf(address.value.attributes.address).call();
-                participant.value = await vault.methods.getParticipant(address.value.attributes.address).call();
-                referrals.value = await vault.methods.getReferrals(address.value.attributes.address).call();
-                walletBalance.value = await token.methods.balanceOf(store.state.wallet.address).call();
+                owned.value = await contract.methods.balanceOf(store.state.wallet.address).call();
+                participant.value = await vault.methods.getParticipant(store.state.wallet.address).call();
                 buyQuantity.value = 15 - owned.value;
                 sellQuantity.value = owned.value;
-                rewardRate.value = await vault.methods.rewardRate(address.value.attributes.address).call() / 100;
-                participantStatus.value = await vault.methods.participantStatus(address.value.attributes.address).call();
             } catch (error) {
                 alerts.danger(error.message);
             }
-            balances.refresh();
             loading.value = false;
-        }
-
-        const toggleNameForm = () => {
-            showUpdateNameForm.value = !showUpdateNameForm.value;
-        }
-
-        const updateName = async () => {
-            const signature = await web3.eth.personal.sign(address.value.attributes.nonce, address.value.attributes.address, "");
-            await axios.post("/api/v1/name", {
-                address: address.value.attributes.address,
-                nonce: address.value.attributes.nonce,
-                signature: signature,
-                name: updatedName.value,
-            }).then(response => {
-                alerts.info("Team name updated");
-            }).catch(error => {
-                if(error.response.status == 422) {
-                    alerts.danger(error.response.data.message);
-                    return;
-                }
-                alerts.danger(error.message);
-            });
-            toggleNameForm();
-            await update();
-        }
-
-        const updateSocialMedia = async () => {
-            const signature = await web3.eth.personal.sign(address.value.attributes.nonce, address.value.attributes.address, "");
-            await axios.post("/api/v1/social", {
-                address: address.value.attributes.address,
-                nonce: address.value.attributes.nonce,
-                signature: signature,
-                twitter: twitter.value,
-                telegram: telegram.value,
-                discord: discord.value,
-                medium: medium.value,
-                facebook: facebook.value,
-                instagram: instagram.value,
-            }).then(response => {
-                alerts.info("Social links updated");
-            }).catch(error => {
-                if(error.response.status == 422) {
-                    alerts.danger(error.response.data.message);
-                    return;
-                }
-                alerts.danger(error.message);
-            });
-            toggleSocialMediaForm();
-            await update();
-
         }
 
         const buy = async () => {
@@ -384,7 +150,7 @@ export default {
             } catch (error) {
                 alerts.danger(error.message);
             }
-            await update();
+            dispatchEvent(new Event("refresh"));
             loading.value = false;
         }
 
@@ -402,71 +168,7 @@ export default {
             } catch (error) {
                 alerts.danger(error.message);
             }
-            await update();
-            loading.value = false;
-        }
-
-        const sendIndividualAirdrop = async () => {
-            const sendAmount = BigInt(individualAirdropAmount.value * 1000000000000000000);
-            if(sendAmount > walletBalance.value) {
-                alerts.danger("Insufficient funds");
-                return;
-            }
-            alerts.warning("waiting on response from wallet");
-            loading.value = true;
-            try {
-                const token = tokenContract();
-                const vault = vaultContract();
-                const gasPriceMultiplier = 1.2;
-                const gasMultiplier = 1.2;
-                const gasPrice = Math.round(await web3.eth.getGasPrice() * gasPriceMultiplier);
-                const allowance = await token.methods.allowance(store.state.wallet.address, store.state.settings.vault_address).call();
-                if(allowance < sendAmount) {
-                    const approveGas = Math.round(await token.methods.approve(store.state.settings.vault_address, sendAmount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                    await token.methods.approve(store.state.settings.vault_address, sendAmount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: approveGas });
-                }
-                const gas = Math.round(await vault.methods.airdrop(address.value.attributes.address, sendAmount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                const result = await vault.methods.airdrop(address.value.attributes.address, sendAmount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
-                alerts.info("Transaction successful! TXID: " + result.blockHash);
-            } catch (error) {
-                alerts.danger(error.message);
-            }
-            await update();
-            loading.value = false;
-        }
-
-        const sendAirdrop = async () => {
-            const sendAmount = BigInt(airdropAmount.value * 1000000000000000000);
-            const min = BigInt(minBalance.value * 1000000000000000000);
-            const max = BigInt(maxBalance.value * 1000000000000000000);
-            if(sendAmount > walletBalance.value) {
-                alerts.danger("Insufficient funds");
-                return;
-            }
-            if(sendAmount < 0) {
-                alerts.danger("Invalid amount");
-                return;
-            }
-            alerts.warning("waiting on response from wallet");
-            loading.value = true;
-            try {
-                const vault = vaultContract();
-                const token = tokenContract();
-                const gasPriceMultiplier = 1.2;
-                const gasMultiplier = 1.2;
-                const gasPrice = Math.round(await web3.eth.getGasPrice() * gasPriceMultiplier);
-                const allowance = await token.methods.allowance(store.state.wallet.address, store.state.settings.vault_address).call();
-                if(allowance < sendAmount) {
-                    const approveGas = Math.round(await token.methods.approve(store.state.settings.vault_address, sendAmount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                    await token.methods.approve(store.state.settings.vault_address, sendAmount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: approveGas });
-                }
-                const gas = Math.round(await vault.methods.airdropTeam(sendAmount, min, max).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                const result = await vault.methods.airdropTeam(sendAmount, min, max).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
-                alerts.info("Transaction successful! TXID: " + result.blockHash);
-            } catch (error) {
-                alerts.danger(error.message);
-            }
-            await update();
+            dispatchEvent(new Event("refresh"));
             loading.value = false;
         }
 
@@ -480,87 +182,21 @@ export default {
             return participant.value[property];
         }
 
-        const toggleUpdateReferrerForm = () => {
-            showUpdateReferrerForm.value = !showUpdateReferrerForm.value;
-        }
-
-        const updateReferrer = async () => {
-            alerts.warning("waiting on response from wallet");
-            loading.value = true;
-            try {
-                const vault = vaultContract();
-                const gasPriceMultiplier = 1.2;
-                const gasMultiplier = 1.2;
-                const gasPrice = Math.round(await web3.eth.getGasPrice() * gasPriceMultiplier);
-                const gas = Math.round(await vault.methods.updateReferrer(newReferrer.value).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                const result = await vault.methods.updateReferrer(newReferrer.value).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
-                alerts.info("Transaction successful! TXID: " + result.blockHash);
-            } catch (error) {
-                alerts.danger(error.message);
-            }
-            await update();
-            loading.value = false;
-        }
-
         return {
             store,
-            name,
-            address,
             loading,
             totalSupply,
             maxSupply,
             owned,
             buyQuantity,
             sellQuantity,
-            teamWallet,
             available,
             buy,
             sell,
-            referrer,
-            referrals,
             directReferrals,
-            rewarded,
-            participantLink,
-            airdropAmount,
-            minBalance,
-            maxBalance,
             walletBalance,
-            sendAirdrop,
-            teamaddress,
-            shortteamaddress,
-            isSelf,
-            toggleNameForm,
-            showUpdateNameForm,
-            updateName,
-            updatedName,
-            participantStatusDisplay,
-            rewardRate,
             displayCurrency,
             getProperty,
-            addr,
-            lookupTeam,
-            individualAirdropAmount,
-            sendIndividualAirdrop,
-            canUpdateReferrer,
-            toggleUpdateReferrerForm,
-            showUpdateReferrerForm,
-            updateReferrer,
-            newReferrer,
-            showSocialMenu,
-            socialLink,
-            showSocialMediaForm,
-            toggleSocialMediaForm,
-            twitter,
-            telegram,
-            discord,
-            medium,
-            facebook,
-            instagram,
-            updateSocialMedia,
-            showImageUploadForm,
-            toggleImageUploadForm,
-            uploadImage,
-            avatar,
         }
     }
 
