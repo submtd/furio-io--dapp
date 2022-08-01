4fbf0f32564317338c218ade15b22d3d853a1c09
Steve Harmeyer <harmeyersteve@gmail.com>
2022-08-01 16:22:57 -0700

    FURB presale

--- resources/js/views/FurbPresale.vue
+++ resources/js/views/FurbPresale.vue
@@ -35,7 +35,7 @@
                     <div class="card h-100">
                         <div class="card-body text-center">
                             <p class="card-title">Purchased</p>
-                            <p class="card-text"><strong>{{ purchased }} out of {{ maxPerWallet }}</strong></p>
+                            <p class="card-text"><strong>{{ purchased }} out of {{ maxPerAddress }}</strong></p>
                         </div>
                     </div>
                 </div>
@@ -51,7 +51,7 @@
                     <div class="card h-100">
                         <div class="card-body text-center">
                             <p class="card-title">Minimum Vault Balance</p>
-                            <p class="card-text"><strong>{{ displayCurrency.format(minVaultBalance) }}$FUR</strong></p>
+                            <p class="card-text"><strong>{{ displayCurrency.format(minVaultBalance) }} $FUR</strong></p>
                         </div>
                     </div>
                 </div>
