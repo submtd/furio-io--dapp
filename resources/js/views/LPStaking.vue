84e3cb4a062414ed8e7a34a6b3531185a9817c2e
Steve Harmeyer <harmeyersteve@gmail.com>
2022-08-17 16:40:59 -0700

    lpstaking

--- resources/js/views/LPStaking.vue
+++ resources/js/views/LPStaking.vue
@@ -128,8 +128,8 @@ export default {
                     const approveGas = Math.round(await payment.methods.approve(store.state.settings.lpstaking_address, amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
                     await payment.methods.approve(store.state.settings.lpstaking_address, amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: approveGas });
                 }
-                const gas = Math.round(await staking.methods.stake(store.state.settings.payment_address, amount, duration.value).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                const result = await staking.methods.stake(store.state.settings.payment_address, amount, duration.value).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
+                const gas = Math.round(await staking.methods.stake(store.state.settings.payment_address, amount, duration.value).estimateGas({ from: store.state.wallet.address, value: "0", gasPrice: gasPrice }) * gasMultiplier);
+                const result = await staking.methods.stake(store.state.settings.payment_address, amount, duration.value).send({ from: store.state.wallet.address, value: "0", gasPrice: gasPrice, gas: gas });
                 alerts.info("Transaction successful! TXID: " + result.blockHash);
                 dispatchEvent(new Event("refresh"));
                 await update();
