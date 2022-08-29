b10ab27c2cb225a6fc0f4fb2ff148b9857e9682a
Steve Harmeyer <harmeyersteve@gmail.com>
2022-08-29 10:35:11 -0700

    bonus claim

--- resources/js/views/Vault.vue
+++ resources/js/views/Vault.vue
@@ -21,6 +21,13 @@
                     <div v-show="lastAction" class="mb-2 text-center">
                         Last action: <strong>{{ lastAction }}</strong>
                     </div>
+                    <div v-show="bonusAvailable" class="mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">You have a bonus claim available!</h5>
+                            </div>
+                        </div>
+                    </div>
                     <div class="row mt-3">
                         <div class="col-6">
                             <button @click="compound" class="btn btn-lg btn-info btn-block">Compound {{ availableDisplay }}</button>
@@ -259,6 +266,7 @@ export default {
         const statusDrop = ref(false);
         const twentyEightDayClaims = ref(0);
         const showRates = ref(false);
+        const bonusAvailable = ref(false);
 
         const stats = ref(null);
         const properties = ref(null);
@@ -409,6 +417,7 @@ export default {
                     referrer.value = participant.value.referrer;
                 }
                 twentyEightDayClaims.value = await contract.methods.twentyEightDayClaims(store.state.wallet.address).call();
+                bonusAvailable.value = await contract.methods.bonusAvailable(store.state.wallet.address).call();
                 const token = tokenContract();
                 balance.value = await token.methods.balanceOf(store.state.wallet.address).call();
                 const autocompound = autocompoundContract();
@@ -517,6 +526,30 @@ export default {
             loading.value = false;
         }
 
+        const claimBonus = async () => {
+            if(!bonusAvailable.value) {
+                alerts.danger("No bonus available");
+                return;
+            }
+            try {
+                const contract = vaultContract();
+                alerts.warning("waiting on response from wallet");
+                loading.value = true;
+                const gasPriceMultiplier = 1;
+                const gasMultiplier = 1.2;
+                const gasPrice = Math.round(await web3.eth.getGasPrice() * gasPriceMultiplier);
+                const gas = Math.round(await contract.methods.claimBonus().estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
+                const result = await contract.methods.claimBonus().send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
+                alerts.info("Transaction successful! TXID: " + result.blockHash);
+            } catch (error) {
+                alerts.danger(error.message);
+            }
+            dispatchEvent(new Event("refresh"));
+            await update();
+            statusDrop.value = false;
+            loading.value = false;
+        }
+
         const cancel = () => {
             statusDrop.value = false;
         }
@@ -614,6 +647,8 @@ export default {
             twentyEightDayClaims,
             showRates,
             toggleRates,
+            bonusAvailable,
+            claimBonus,
         }
     }
 }
