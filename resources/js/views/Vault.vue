84d0281830e69f7b01ad4a5cdd127cc2f6ae5cb7
Steve Harmeyer <harmeyersteve@gmail.com>
2022-08-29 10:40:22 -0700

    bonus claim

--- resources/js/views/Vault.vue
+++ resources/js/views/Vault.vue
@@ -25,7 +25,7 @@
                         <div class="card text-white bg-info">
                             <div class="card-body">
                                 <h5 class="card-title text-center">You have a bonus claim available!</h5>
-                                <button @click="claimBonus" class="btn btn-lg btn-primary btn-block mb-2">Claim Bonus</button>
+                                <button @click="claimBonus" class="btn btn-lg btn-success btn-block mb-2">Claim Bonus</button>
                             </div>
                         </div>
                     </div>
