d6167fe2f042f290b764235d65b16ac5cf54f7fa
Steve Harmeyer <harmeyersteve@gmail.com>
2022-09-10 09:08:53 -0700

    swap updates

--- resources/js/views/Swap.vue
+++ resources/js/views/Swap.vue
@@ -1,89 +1,91 @@
 <template>
-    <h1>Furswap</h1>
-    <p class="mb-5">Swap USDC for FUR tokens here.<br/>
-                    FUR token runs the Furio ecosystem.<br/>
-                    Earn up to 2.5% daily rewards by depositing your tokens in to Furvault.</p>
-    <div class="row flex-row-reverse gx-5">
-        <div class="col-lg-7 bg-light text-dark rounded p-5 mb-4">
-            <div v-show="loading" class="text-center">
-                <div class="spinner-border m-5" role="status">
-                    <span class="sr-only">Loading...</span>
+    <div>
+        <h1>Furswap</h1>
+        <p class="mb-5">Swap USDC for FUR tokens here.<br/>
+                        FUR token runs the Furio ecosystem.<br/>
+                        Earn up to 2.5% daily rewards by depositing your tokens in to Furvault.</p>
+        <div class="row flex-row-reverse gx-5">
+            <div class="col-lg-7 bg-light text-dark rounded p-5 mb-4">
+                <div v-show="loading" class="text-center">
+                    <div class="spinner-border m-5" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                </div>
+                <div v-show="!loading">
+                    <p>Current $FUR price: <strong>{{ price }} USDC</strong></p>
+                    <div class="form-group">
+                        <label for="from">From</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><strong>{{ fromCurrency }}</strong></div>
+                            </div>
+                            <input v-model="from" class="form-control" id="from"/>
+                        </div>
+                        <div class="text-right">
+                            <button @click="max" class="btn btn-link"><small class="form-text text-muted">max</small></button>
+                        </div>
+                    </div>
+                    <div v-show="warning" class="alert alert-danger">
+                        <strong>{{ warning }}</strong>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="to">To</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><strong>{{ toCurrency }}</strong></div>
+                            </div>
+                            <input v-model="output" class="form-control" id="to" disabled/>
+                        </div>
+                    </div>
+                    <div v-show="showVault" class="form-group">
+                        <div class="form-check">
+                            <input v-model="vault" class="form-check-input" type="checkbox" id="vault"/>
+                            <label for="vault" class="form-check-label">Deposit directly into the <router-link :to="{ name: 'Vault' }"><strong>Vault</strong></router-link></label>
+                        </div>
+                    </div>
+                    <div v-show="showVault && vault && showReferrer" class="form-group">
+                        <div v-show="!referrer">
+                            <div class="alert alert-danger">
+                                <strong>You do not currently have a referrer set. By leaving this field blank, your referrer will default to the dev wallet.</strong>
+                            </div>
+                        </div>
+                        <label for="referrer">Referrer</label>
+                        <input v-model="referrer" class="form-control" id="referrer"/>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-10">
+                            <div v-show="store.state.wallet.loggedIn">
+                                <button @click="swap" class="btn btn-lg btn-info btn-block">Swap</button>
+                            </div>
+                            <div v-show="!store.state.wallet.loggedIn">
+                                <button @click="swap" class="btn btn-lg btn-info btn-block" data-toggle="modal" data-target="#loginmodal">Connect Wallet</button>
+                            </div>
+                            <LoginModal/>
+                        </div>
+                        <div class="col-2">
+                            <button @click="swapToFrom" class="btn btn-lg btn-secondary btn-block"><i class="bi bi-arrow-down-up"></i></button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div v-show="!loading">
-                <p>Current $FUR price: <strong>{{ price }} USDC</strong></p>
-                <div class="form-group">
-                    <label for="from">From</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><strong>{{ fromCurrency }}</strong></div>
-                        </div>
-                        <input v-model="from" class="form-control" id="from"/>
-                    </div>
-                    <div class="text-right">
-                        <button @click="max" class="btn btn-link"><small class="form-text text-muted">max</small></button>
-                    </div>
-                </div>
-                <div v-show="warning" class="alert alert-danger">
-                    <strong>{{ warning }}</strong>
-                </div>
-                <div class="form-group mb-3">
-                    <label for="to">To</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><strong>{{ toCurrency }}</strong></div>
-                        </div>
-                        <input v-model="output" class="form-control" id="to" disabled/>
-                    </div>
-                </div>
-                <div v-show="showVault" class="form-group">
-                    <div class="form-check">
-                        <input v-model="vault" class="form-check-input" type="checkbox" id="vault"/>
-                        <label for="vault" class="form-check-label">Deposit directly into the <router-link :to="{ name: 'Vault' }"><strong>Vault</strong></router-link></label>
-                    </div>
-                </div>
-                <div v-show="showVault && vault && showReferrer" class="form-group">
-                    <div v-show="!referrer">
-                        <div class="alert alert-danger">
-                            <strong>You do not currently have a referrer set. By leaving this field blank, your referrer will default to the dev wallet.</strong>
+            <div class="col-lg-5">
+                <div class="row">
+                    <div class="col-lg-6 mb-4">
+                        <div class="card h-100">
+                            <div class="card-body text-center">
+                                <img src="../../images/usdc.svg" class="mx-auto d-block mb-3" alt="USDC" width="75" height="75"/>
+                                <p class="card-title">USDC Balance</p>
+                                <p class="card-text"><strong>{{ store.state.balances.payment }} USDC</strong></p>
+                            </div>
                         </div>
                     </div>
-                    <label for="referrer">Referrer</label>
-                    <input v-model="referrer" class="form-control" id="referrer"/>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-10">
-                        <div v-show="store.state.wallet.loggedIn">
-                            <button @click="swap" class="btn btn-lg btn-info btn-block">Swap</button>
-                        </div>
-                        <div v-show="!store.state.wallet.loggedIn">
-                            <button @click="swap" class="btn btn-lg btn-info btn-block" data-toggle="modal" data-target="#loginmodal">Connect Wallet</button>
-                        </div>
-                        <LoginModal/>
-                    </div>
-                    <div class="col-2">
-                        <button @click="swapToFrom" class="btn btn-lg btn-secondary btn-block"><i class="bi bi-arrow-down-up"></i></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-5">
-            <div class="row">
-                <div class="col-lg-6 mb-4">
-                    <div class="card h-100">
-                        <div class="card-body text-center">
-                            <img src="../../images/usdc.svg" class="mx-auto d-block mb-3" alt="USDC" width="75" height="75"/>
-                            <p class="card-title">USDC Balance</p>
-                            <p class="card-text"><strong>{{ store.state.balances.payment }} USDC</strong></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card h-100">
-                        <div class="card-body text-center">
-                            <img src="../../images/fur.svg" class="mx-auto d-block mb-3" alt="FUR" width="75" height="75"/>
-                            <p class="card-title">$FUR Balance</p>
-                            <p class="card-text"><strong>{{ store.state.balances.token }} $FUR</strong></p>
+                    <div class="col-lg-6 mb-4">
+                        <div class="card h-100">
+                            <div class="card-body text-center">
+                                <img src="../../images/fur.svg" class="mx-auto d-block mb-3" alt="FUR" width="75" height="75"/>
+                                <p class="card-title">$FUR Balance</p>
+                                <p class="card-text"><strong>{{ store.state.balances.token }} $FUR</strong></p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -157,7 +159,7 @@ export default {
                 const token = new web3.eth.Contract(JSON.parse(store.state.settings.token_abi), store.state.settings.token_address);
                 participant.value = await vault.methods.getParticipant(store.state.wallet.address).call();
                 price.value = displayCurrency.format(await swap.methods.sellOutput("1000000000000000000").call());
-                onCooldown.value = await token.methods.onCooldown(store.state.wallet.address).call();
+                onCooldown.value = await swap.methods.onCooldown(store.state.wallet.address).call();
                 balances.refresh();
             } catch (error) {
                 alerts.danger(error.message);
@@ -211,7 +213,7 @@ export default {
                     output.value = displayCurrency.format(await swap.methods.sellOutput(amount).call());
                 }
                 if(fromCurrency.value == "USDC") {
-                    output.value = displayCurrency.format(await swap.methods.buyOutput(amount).call());
+                    output.value = displayCurrency.format(await swap.methods.buyOutput(store.state.settings.payment_address, amount).call());
                 }
             } catch (error) {
                 alerts.danger(error.message);
@@ -251,15 +253,19 @@ export default {
                 let result;
                 if(fromCurrency.value == "USDC" && vault.value == true) {
                     if(referrer.value) {
-                        gas = Math.round(await swap.methods.depositBuy(amount, referrer.value).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                        result = await swap.methods.depositBuy(amount, referrer.value).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
+                        gas = Math.round(await swap.methods.depositBuy(store.state.settings.payment_address, amount, referrer.value).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
+                        result = await swap.methods.depositBuy(store.state.settings.payment_address, amount, referrer.value).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
                     } else {
-                        gas = Math.round(await swap.methods.depositBuy(amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                        result = await swap.methods.depositBuy(amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
+                        gas = Math.round(await swap.methods.depositBuy(store.state.settings.payment_address, amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
+                        result = await swap.methods.depositBuy(store.state.settings.payment_address, amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
                     }
-                } else {
-                    gas = Math.round(await swap.methods[method](amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                    result = await swap.methods[method](amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
+                } else if(fromCurrency.value == "USDC") {
+                    gas = Math.round(await swap.methods.buy(store.state.settings.payment_address, amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
+                    result = await swap.methods.buy(store.state.settings.payment_address, amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
+                }
+                else {
+                    gas = Math.round(await swap.methods.sell(amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
+                    result = await swap.methods.sell(amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
                 }
                 alerts.info("Transaction successful! TXID: " + result.blockHash);
             } catch (error) {
