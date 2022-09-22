c3c23d58d09b43115d9f1ec70461afb6025ba4a5
Steve Harmeyer <harmeyersteve@gmail.com>
2022-09-22 20:10:16 +0000

    fix buy on swap

--- resources/js/views/Swap.vue
+++ resources/js/views/Swap.vue
@@ -265,9 +265,13 @@ export default {
                         gas = Math.round(await swap.methods.depositBuy(store.state.settings.payment_address, amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
                         result = await swap.methods.depositBuy(store.state.settings.payment_address, amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
                     }
-                } else {
-                    gas = Math.round(await swap.methods[method](amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
-                    result = await swap.methods[method](amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
+                } else if(fromCurrency.value == "USDC") {
+                    gas = Math.round(await swap.methods.buy(store.state.settings.payment_address, amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
+                    result = await swap.methods.buy(store.state.settings.payment_address, amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
+                }
+                else {
+                    gas = Math.round(await swap.methods.sell(amount).estimateGas({ from: store.state.wallet.address, gasPrice: gasPrice }) * gasMultiplier);
+                    result = await swap.methods.sell(amount).send({ from: store.state.wallet.address, gasPrice: gasPrice, gas: gas });
                 }
                 alerts.info("Transaction successful! TXID: " + result.blockHash);
             } catch (error) {
